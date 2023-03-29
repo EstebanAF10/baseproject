7c07c34698348cb08a0afbf1b73735bcf2f5d0dc
Esteban Arias <esteban.arfu@gmail.com>
2023-03-29 03:34:39 +0000

    Added Google Analytics Module

--- project/web/sites/default/settings.ddev.php
+++ project/web/sites/default/settings.ddev.php
@@ -28,7 +28,7 @@ $databases['default']['default'] = array(
   'prefix' => "",
 );
 
-$settings['hash_salt'] = 'NcSsxVSRITrMhgENDMeypDFzlUszaUTkwRSYcpaFiYOcDjVYFSGnTdryCMwxayot';
+$settings['hash_salt'] = 'pRNZxgqVPnaCkVMTGMgApYCWtgIfKFzvPNGxLKjZaGyyFDpFNkcWTMgwMDHesQJx';
 
 // This will prevent Drupal from setting read-only permissions on sites/default.
 $settings['skip_permissions_hardening'] = TRUE;
