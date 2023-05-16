c2c359bada05398ca3c42797bca131fde9db4e82
Esteban Arias <esteban.arfu@gmail.com>
2023-05-16 00:55:07 +0000

    Layout builder

--- project/web/sites/default/settings.ddev.php
+++ project/web/sites/default/settings.ddev.php
@@ -28,7 +28,7 @@ $databases['default']['default'] = array(
   'prefix' => "",
 );
 
-$settings['hash_salt'] = 'bxODppgRnOmCVaMXJxYtUmRsyrjxjWDLTKpBxrIdkwZIjFloTvYhIgrqfuOsMbVx';
+$settings['hash_salt'] = 'cBiwRwmqAUSpEUhAMBkhrNFRwxrKKuFXBsCONBrcNdeiSxdiREYmbDXmxoIGkZdR';
 
 // This will prevent Drupal from setting read-only permissions on sites/default.
 $settings['skip_permissions_hardening'] = TRUE;
